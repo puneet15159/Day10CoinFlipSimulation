057639d3af4aa33b744f9b00bbea3edbc035488a
puneet <puneet15159@iiitd.ac.in>
2020-06-05 13:30:06 +0530

    Loop added

--- flipCoinSimulater.sh
+++ flipCoinSimulater.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-flip=$((RANDOM%2))
+counter=10
 
+while (( counter > 0 ))
+do
+flip=$((RANDOM%2))
 if [[ flip -eq 0 ]]
 then
 	echo "heads"
 else
 	echo "tails"
 fi
+
+((counter--))
+done
