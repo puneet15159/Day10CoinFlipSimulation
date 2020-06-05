797e4f4d55a856d64806fb2f6e513498eef2dd1e
puneet <puneet15159@iiitd.ac.in>
2020-06-05 13:40:51 +0530

    winner decision

--- flipCoinSimulater.sh
+++ flipCoinSimulater.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-counter=10
-
-while (( counter > 0 ))
+countertails=0
+counterheads=0
+while (( 1 ))
 do
-flip=$((RANDOM%2))
-if [[ flip -eq 0 ]]
-then
-	echo "heads"
-else
-	echo "tails"
-fi
+ flip=$((RANDOM%2))
+ if [[ flip -eq 0 ]]
+ then
+	#echo "heads"
+	((counterheads++))
+ else
+	#echo "tails"
+	((countertails++))
+ fi
 
-((counter--))
+ if [[ counterheads -eq 21 ]] || [[ countertails -eq 21 ]]
+ then
+ 	break
+ fi
+
 done
+
+echo "head count: $counterheads"
+echo "tails count: $countertails"
+
+if [[ counterheads -gt countertails ]]
+then
+	result=$(($counterheads-$countertails))
+        echo "heads won by: $result"
+elif [[ countertails -gt counterheads ]]
+then
+	result=$(($countertails-$counterheads))
+	echo "tails won by: $result"
+else
+
+	echo "its a tie"
+fi
